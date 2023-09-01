0bea71c25eb9337a853f0409649c194faf438e94
Ayan Sheikh <114312039+Ayan-sh03@users.noreply.github.com>
2023-09-01 20:23:30 +0530

    cors

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'corsheaders'
     'django.contrib.staticfiles',
     'rest_framework'
 ]
@@ -39,9 +40,16 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+ "*"
+ ]
+
 
 ROOT_URLCONF = 'urlShortner.urls'
 
